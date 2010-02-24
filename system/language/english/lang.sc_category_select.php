8a1e78153a750567e2fa07b9c7d30a36883b056f
Andrew Gunstone <andrew@thirststudios.com>
2010-02-24 12:35:03 +1100

    Initial release

---
+++ system/language/english/lang.sc_category_select.php
@@ -0,0 +1,14 @@
+<?php
+
+$L = array(
+
+
+// Field Settings
+'select_category_group' =>
+'Select a category group',
+
+''=>''
+);
+
+/* End of file lang.sc_category_select.php */
+/* Location: ./system/language/english/lang.sc_category_select.php */
